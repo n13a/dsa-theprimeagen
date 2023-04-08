89a5f83d836100671b08107798c8949c306cd392
Nima <nima97e@gmail.com>
2023-04-08 20:13:14 +0330

    implement binary search algorithm

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,3 +1,18 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-    
+    let low = 0
+    let high = haystack.length
+
+    do {
+        const m = Math.floor(low + (high - low) / 2)
+        const v = haystack[m]
+        if (v === needle) {
+            return true
+        } else if (v > needle) {
+            high = m
+        } else {
+            low = m + 1
+        }
+
+    } while (low < high);
+    return false
 }
