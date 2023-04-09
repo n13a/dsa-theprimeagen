53a18d528fcef996da00a883bf0cf86734866905
Nima <nima97e@gmail.com>
2023-04-09 20:15:06 +0330

    implement bubble sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,12 @@
 export default function bubble_sort(arr: number[]): void {
-
+    for(let i = 0; i < arr.length; i++){
+        for(let j = 0; j < arr.length - 1 - i; j++){
+            const cur = arr[j]
+            const next = arr[j+1]
+            if(cur > next){
+                arr[j] = next
+                arr[j+1] = cur
+            }
+        }
+    }
 }
