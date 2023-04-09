9af2531a4400d23611e03a4f3f12b4ee00c39015
Nima <nima97e@gmail.com>
2023-04-09 17:30:01 +0330

    solve two crystal problem

--- src/day1/TwoCrystalBalls.ts
+++ src/day1/TwoCrystalBalls.ts
@@ -1,3 +1,13 @@
 export default function two_crystal_balls(breaks: boolean[]): number {
+    let step = Math.floor(Math.sqrt(breaks.length))
 
+    for(var i = step; i < breaks.length; i+= step){
+        if(breaks[i]) break
+    }
+    i -= step
+    
+    for(let j = 0; j < step && i < breaks.length; j++, i++){
+        if(breaks[i]) return i
+    }
+    return -1
 }
