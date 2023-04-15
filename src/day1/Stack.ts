31f21dac8bc47e53c41d0faa6ba27a501cac80c6
Nima <nima97e@gmail.com>
2023-04-15 22:31:38 +0330

    implement stack

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,39 @@
+type Node<T> = {
+    value: T
+    next?: Node<T>
+}
+
 export default class Stack<T> {
     public length: number;
+    private head?: Node<T>
 
-    
+
 
     constructor() {
+        this.head = undefined
+        this.length = 0
     }
 
     push(item: T): void {
-
-}
+        this.length++
+        const node = { value: item } as Node<T>
+        if (!this.head) {
+            this.head = node
+            return
+        }
+        node.next = this.head
+        this.head = node
+    }
     pop(): T | undefined {
-
-}
+        if (!this.head) {
+            return undefined
+        }
+        this.length--
+        const head = this.head
+        this.head = this.head.next
+        return head.value
+    }
     peek(): T | undefined {
-
-}
+        return this.head?.value
+    }
 }
