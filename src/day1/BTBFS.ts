b49590dff559ae8ca72bf1c35c7ecb744c2d5527
Nima <nima97e@gmail.com>
2023-05-16 21:32:42 +0330

    implement binary tree breadth-first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,3 +1,19 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
+  const q = [head]
+
+  while(q.length){
+    const curr = q.shift() as BinaryNode<number>
+    // search
+    if(curr.value === needle){
+      return true
+    } 
+    if(curr.left){
+      q.push(curr.left)
+    }
+    if(curr.right){
+      q.push(curr.right)
+    }
+  }
+  return false
 
 }
