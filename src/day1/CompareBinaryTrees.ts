535d210707c2649045a58180d85e547d01332c1f
Nima <nima97e@gmail.com>
2023-05-18 00:46:13 +0330

    implement compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,15 @@
 export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+  if(a === null && a === null){
+    return true
+  }
 
+  if(a === null || b === null){
+    return false
+  }
+  
+  if(a.value !== b.value){
+    return false
+  }
+
+  return compare(a.left, b.left) && compare(a.right, b.right)
 }
