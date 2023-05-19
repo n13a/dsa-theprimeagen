4c336ec8af66e86ef90c9612c82a7323a26b3b15
Nima <nima97e@gmail.com>
2023-05-19 19:46:38 +0330

    implement depth first search on binary search tree

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,3 +1,19 @@
-export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+function search(curr: BinaryNode<number> | null, needle: number): boolean {
+  if(!curr){
+    return false
+  }
 
+  if(curr.value === needle){
+    return true
+  }
+
+  if(curr.value < needle){
+    return search(curr.right, needle)
+  }
+
+  return search(curr.left, needle)
+
+}
+export default function dfs(head: BinaryNode<number>, needle: number): boolean {
+  return search(head, needle)
 }
