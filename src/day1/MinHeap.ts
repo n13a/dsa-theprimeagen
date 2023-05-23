509e89da7fe93177b231cfec55c21461d20c492b
Nima <nima97e@gmail.com>
2023-05-23 16:26:58 +0330

    implement min heap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,82 @@
 export default class MinHeap {
-    public length: number;
+  public length: number;
+  private data: number[]
 
-    
+  constructor() {
+    this.data = []
+    this.length = 0
+  }
 
-    constructor() {
+  insert(value: number): void {
+    this.data[this.length] = value
+    this.heapifyUp(this.length)
+    this.length++
+  }
+
+  delete(): number {
+    if(this.length === 0){
+      return -1
     }
+    const out = this.data[0]
+    this.length--
 
-    insert(value: number): void {
+    if(this.length === 0){
+      this.data = []
+      return out
+    }
+    this.data[0] = this.data[this.length]
+    this.heapifyDown(0)
+    return out
+  }
 
-}
-    delete(): number {
+  private heapifyDown(idx: number): void{
+    const lIdx = this.leftChild(idx)
+    const rIdx = this.rightChild(idx)
 
-}
+    if(idx >= this.length || lIdx >= this.length){
+      return
+    }
+    const lV = this.data[lIdx]
+    const rV = this.data[rIdx]
+    const v = this.data[idx]
+
+    if(lV > rV && v > rV){
+      this.data[idx] = rV
+      this.data[rIdx] = v
+      this.heapifyDown(rIdx)
+    } else if(lV < rV && v > lV){
+      this.data[idx] = lV
+      this.data[lIdx] = v
+      this.heapifyDown(lIdx)
+    }
+  }
+
+  private heapifyUp(idx: number): void{
+    if(idx === 0){
+      return
+    }
+    const p = this.parent(idx)
+    const parentV = this.data[p]
+    const v = this.data[idx]
+
+    if(parentV > v){
+      this.data[idx] = parentV
+      this.data[p] = v
+
+      this.heapifyUp(p)
+    }
+  }
+
+
+  private parent(idx: number): number {
+    return Math.floor((idx - 1) / 2)
+  }
+
+  private leftChild(idx: number): number {
+    return idx * 2 + 1
+  }
+
+  private rightChild(idx: number): number {
+    return idx * 2 + 2
+  }
 }
